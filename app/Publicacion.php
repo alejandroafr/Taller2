9003e33d5080f95ae241f4facd494ee36557a36f
alejandroafr <alejandrofernandez@estudiante.uniajc.edu.co>
2020-11-17 21:37:12 -0500

    Primer commit Taller2

---
+++ app/Publicacion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Publicacion extends Model
+{
+    protected $table = ['publicaciones'];
+    protected $fillable=['titulo','cuerpo'];
+    public $timestamps=false;
+
+    //Relacion con la tabla usuario
+
+    public function usuarios()
+    {
+        return $this->belongsTo('App\Usuario');
+    }
+}
